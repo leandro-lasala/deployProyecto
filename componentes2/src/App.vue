1bc7decfbd52a9ce9c84e2a19ab00eab38594fa2
Leandro Bautista Lasala <167924578+leandro-lasala@users.noreply.github.com>
2026-05-12 22:13:53 -0300

    Integracion app -Prueba

--- componentes2/src/App.vue
+++ componentes2/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Prueba.vue from '/components/Prueba.vue'
 import SumBoton from './components/SumBoton.vue'
 </script>
 
 <template>
   <SumBoton />
   <HelloWorld />
+  <Prueba />
 </template>
