2f5976a98159dce7b9b222560d35e4563b7a4533
Leandro Bautista Lasala <167924578+leandro-lasala@users.noreply.github.com>
2026-05-12 22:24:32 -0300

    Correcion

--- componentes2/src/App.vue
+++ componentes2/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import Prueba.vue from '/components/Prueba.vue'
+import Prueba from '/components/Prueba.vue'
 import SumBoton from './components/SumBoton.vue'
 </script>
 
