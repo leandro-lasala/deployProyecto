b70c51935914c873ac48c5d6bf817695cb39a6b6
Leandro Bautista Lasala <167924578+leandro-lasala@users.noreply.github.com>
2026-05-12 22:27:33 -0300

    .

--- componentes2/src/App.vue
+++ componentes2/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import Prueba from '/components/Prueba.vue'
+import Prueba from './components/Prueba.vue'
 import SumBoton from './components/SumBoton.vue'
 </script>
 
